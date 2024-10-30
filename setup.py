91dfee3bd07fc91e6b3b51cf46c5a36372196b1a
Igor Rodrigues <igorrss652@gmail.com>
2024-10-30 12:46:59 -0300

    WIP

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from setuptools import setup, find_packages
 
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="foo",
     version="1.0",
     packages=find_packages(),
+    install_requires=required,
     entry_points = {
         'console_scripts': [
             'vegeta-charts = vegeta_charts.main:main',                  
